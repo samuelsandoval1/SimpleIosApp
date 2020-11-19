8ad3e87e68e1c2bcb4cb764998c78b10f5655e78
Samuel Sandoval <sjsandoval1000@gmail.com>
2020-11-19 13:12:50 -0800

    Initial Commit

---
+++ SimpleiOSApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpleiOSApp
+//
+//  Created by Samuel Sandoval on 11/19/20.
+//  Copyright © 2020 Samuel Sandoval. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
